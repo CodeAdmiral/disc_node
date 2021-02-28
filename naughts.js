a1ddfc63affc540c0e6cf390c06229eaa3834572
dotNetCory <Cory.Forster@moneypenny.com>
2021-02-28 00:30:36 +0000

    First commit

---
+++ naughts.js
@@ -0,0 +1,68 @@
+class naughts {
+    grid;
+    playerX = '';
+    playerO = '';
+    lastPlayed = 'X';
+    nextPlay = '';
+
+    constructor(){
+        this.reset();
+    }
+
+    move(location){
+        if(this.lastPlayed == 'O'){
+            this.grid[location] = '  X  ';
+            this.nextPlay = this.playerO;
+            this.lastPlayed = 'X'
+        }else if(this.lastPlayed == 'X'){
+            this.grid[location] = '  O  ';
+            this.nextPlay = this.playerX;
+            this.lastPlayed = 'O'
+        }
+    }
+
+    setPlayerX(inputID){
+        if(this.playerX != '')
+            return true;
+        this.playerX = inputID;
+        if(this.nextPlay == ''){
+            this.nextPlay = inputID;
+        }
+        return false;
+    }
+
+    setPlayerO(inputID){
+        if(this.playerO != '')
+            return true;
+        this.playerO = inputID;
+        if(this.nextPlay == ''){
+            this.nextPlay = inputID;
+        }
+        return false;
+    }
+
+    reset(){
+        this.grid =
+        { 
+            a1: '        ', a2: '        ', a3: '        ',
+            b1: '        ', b2: '        ', b3: '        ',
+            c1: '        ', c2: '        ', c3: '        '    
+        }
+        this.lastPlayed = 'X';
+        this.playerX = '';
+        this.playerO = '';
+        this.nextPlay = '';
+    }
+
+    show(){
+        var output = '.     |    1    |    2    |    3      |\n' +
+        'A   | ' + this.grid.a1 + ' | ' + this.grid.a2  + ' | ' + this.grid.a3 + '   |\n' +
+        '      |---------------------|' + ' \n' +
+        'B   | ' + this.grid.b1 + ' | ' + this.grid.b2  + ' | ' + this.grid.b3 + '   |\n' +
+        '      |---------------------|' + ' \n' +
+        'C   | ' + this.grid.c1 + ' | ' + this.grid.c2  + ' | ' + this.grid.c3 + '   |\n' 
+        return output;
+    }
+}
+
+module.exports = naughts
